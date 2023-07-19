19a2dccdf5d3b61b6c1fd725fc18264d9d39ddfc
ajaysehwal <ajaysehwal000@gmail.com>
2023-07-19 22:02:16 +0530

    updated

--- client/src/components/CardThree.tsx
+++ client/src/components/CardThree.tsx
@@ -3,15 +3,10 @@ const CardThree = () => {
     <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
   
-        <svg
-          className="fill-primary dark:fill-white"
-          width="22"
-          height="22"
-          viewBox="0 0 22 22"
-         xmlns="http://www.w3.org/2000/svg" fill="none"  stroke-width="1.5" stroke="currentColor" >
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
-</svg>
-
+       
+<svg className="h-6 w-6"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor"
+ fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/> 
+  <polyline points="3 17 9 11 13 15 21 7" />  <polyline points="14 7 21 7 21 14" /></svg>
       </div>
 
       <div className="mt-4 flex items-end justify-between">
