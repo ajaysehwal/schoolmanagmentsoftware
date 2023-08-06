1e2ed34fae2b9d539327880cdd379df34418e507
ajaysehwal <ajaysehwal000@gmail.com>
2023-08-06 12:25:02 +0530

    updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,10 +9,12 @@ import Schooldetails from './pages/Authentication/signupsteps/register_school';
 import AddressDetails from './pages/Authentication/signupsteps/register_address';
 import Admindata from './pages/Authentication/signupsteps/finalstep';
 import LoginLoadingpage from './pages/Authentication/signupsteps/loadingpage';
-const Calendar = lazy(() => import('./pages/Calendar'));
+const SchoolCalendar = lazy(() => import('./pages/Calendar'));
 const Chart = lazy(() => import('./pages/Chart'));
+const Studenttable=lazy(()=>import('./components/studenttable'));
 const FormElements = lazy(() => import('./pages/Form/FormElements'));
 const Studentadmissionform=lazy(()=>import('./components/studentadmissionform'));
+const AddStudentHouse=lazy(()=>import('./components/AddStudentHouse'));
 const FormLayout = lazy(() => import('./pages/Form/FormLayout'));
 const Profile = lazy(() => import('./pages/Profile'));
 const Settings = lazy(() => import('./pages/Settings'));
@@ -20,8 +22,21 @@ const Tables = lazy(() => import('./pages/Tables'));
 const Alerts = lazy(() => import('./pages/UiElements/Alerts'));
 const Buttons = lazy(() => import('./pages/UiElements/Buttons'));
 const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
-
+const AddClasses=lazy(()=>import('./components/addclasses'));
+const TearcherAdmissionform=lazy(()=>import('./components/Addteachers'));
+const Teacherdetailtable=lazy(()=>import('./components/teacherstable'));
+const Teacherspage =lazy(()=>import('./components/teacherspage'));
+const Createtimetable=lazy(()=>import('./components/createtimetable'));
+const FileUpload=lazy(()=>import('./components/try'));
+const Studentdetailspage=lazy(()=>import('./components/studentdetailspage'));
+const AddClasses_section=lazy(()=>import('./components/addclass_section'))
+const Schooltimetable=lazy(()=>import('./components/schooltimetable'));
+const Createsubject=lazy(()=>import('./components/createsubject'));
+const Teacherdetailpage=lazy(()=>import('./components/teacherdetailpage'));
+const AddSchoolHoilday=lazy(()=>import('./components/AddSchoolholiday'));
+const Teachertimetable=lazy(()=>import('./components/teachertimetable'));
 function App() {
+ 
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -36,7 +51,7 @@ function App() {
       <Route path='/home' element={<Home/>} />
       <Route path='/admin_account' element={<Admindata/>}/>
       <Route path='/loading' element={<LoginLoadingpage/>}/>
-
+    
        <Route path='/register_address' element={<AddressDetails/>}/>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
@@ -47,7 +62,7 @@ function App() {
             path="/calendar"
             element={
               <Suspense fallback={<Loader />}>
-                <Calendar />
+                <SchoolCalendar />
               </Suspense>
             }
           />
@@ -60,10 +75,123 @@ function App() {
             }
           />
           <Route
-            path="/studentadmission"
+            path="/students/admissionform"
             element={
               <Suspense fallback={<Loader />}>
                 <Studentadmissionform />
+              </Suspense>
+            }
+          />
+            <Route
+            path="/academics/studentclasses"
+            element={
+              <Suspense fallback={<Loader />}>
+                <AddClasses/>
+              </Suspense>
+            }
+          />
+            <Route
+            path="/academics/createsubjects"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Createsubject/>
+              </Suspense>
+            }
+          />
+            <Route
+            path="/try"
+            element={
+              <Suspense fallback={<Loader />}>
+                <FileUpload/>
+              </Suspense>
+            }
+          />
+
+            <Route
+            path="/academics/classes_sections"
+            element={
+              <Suspense fallback={<Loader />}>
+                <AddClasses_section/>
+              </Suspense>
+            }
+          />
+           <Route
+            path="/students/studentdetails/:id"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Studentdetailspage/>
+              </Suspense>
+            }
+          />
+           <Route
+            path="/setting/studenthouses"
+            element={
+              <Suspense fallback={<Loader />}>
+             <AddStudentHouse/>
+              </Suspense>
+            }
+          />
+           <Route
+            path="/students/detailtable"
+            element={
+              <Suspense fallback={<Loader />}>
+              <Studenttable/>
+              </Suspense>
+            }
+          />
+            <Route
+            path="/human_resoures/add_teachers"
+            element={
+              <Suspense fallback={<Loader />}>
+              <TearcherAdmissionform/>
+              </Suspense>
+            }
+          />
+             <Route
+            path="/human_resoures/teaching-staff/:id"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Teacherdetailpage/>
+              </Suspense>
+            }
+          />
+            <Route
+            path="/human_resoures/teaching-staff"
+            element={
+              <Suspense fallback={<Loader />}>
+              <Teacherspage/>
+              </Suspense>
+            }
+          />
+            <Route
+            path="/academics/createtimetable"
+            element={
+              <Suspense fallback={<Loader />}>
+              <Createtimetable/>
+              </Suspense>
+            }
+          />
+            <Route
+            path="/academics/addschoolholiday"
+            element={
+              <Suspense fallback={<Loader />}>
+              <AddSchoolHoilday/>
+              </Suspense>
+            }
+          />
+           <Route
+            path="/academics/schooltimetable"
+            element={
+              <Suspense fallback={<Loader />}>
+              <Schooltimetable/>
+              </Suspense>
+            }
+          />
+           <Route
+            path="/academics/schooltimetable/:teacher_id"
+            element={
+              <Suspense fallback={<Loader />}>
+              <Teachertimetable/>
               </Suspense>
             }
           />
@@ -72,6 +200,14 @@ function App() {
             element={
               <Suspense fallback={<Loader />}>
                 <FormElements />
+              </Suspense>
+            }
+          />
+              <Route
+            path="/teacherstable"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Teacherdetailtable/>
               </Suspense>
             }
           />
