675250f8496570deb529fc4116fd5811e9728713
ajaysehwal <ajaysehwal000@gmail.com>
2023-08-11 21:09:25 +0530

    updated

--- client/src/components/CardSix.tsx
+++ client/src/components/CardSix.tsx
@@ -1,4 +1,49 @@
+import { Spinner } from "@material-tailwind/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
+
 const CardSix = () => {
+  const cookies = new Cookies();
+  const auth = cookies.get('_UID');
+  const [male,setmale]=useState(0);
+  const [load,setload]=useState(false);
+  const [female,setfemale]=useState(0);
+  const [other,setother]=useState(0);
+
+  const getdatacount=async(auth:any)=>{
+    setload(true);
+    try{
+      const res=await axios.get(`http://localhost:8000/schoolteacher/${auth}/male`)
+        setmale(res.data.length);
+        setload(false);
+    }catch(err){
+      return err;
+    }
+    try{
+       setload(true);
+      const res=await axios.get(`http://localhost:8000/schoolteacher/${auth}/female`)
+   setfemale(res.data.length)
+   setload(false);
+
+    }catch(err){
+      return err;
+    }
+    try{
+      setload(true);
+
+      const res=await axios.get(`http://localhost:8000/schoolteacher/${auth}/other`)
+   setother(res.data.length)
+   setload(false);
+
+    }catch(err){
+      return err;
+    }
+  }
+  useEffect(()=>{
+ getdatacount(auth);
+  },[])
+
     return (
       <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
         {/* <div className="flex h-15.5 w-15.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4"> */}
@@ -14,7 +59,7 @@ const CardSix = () => {
   </svg>
    */}
 
-   <p className="text-title-md font-bold text-black dark:text-white">423</p>
+   <p className="text-title-md font-bold text-black dark:text-white">{load?<Spinner/>:male+female+other}</p>
         {/* </div> */}
   
         <div className="mt-4 flex items-end justify-between">
@@ -45,19 +90,19 @@ const CardSix = () => {
             </svg>
           </span> */}
         </div>
-        <div style={{display:"grid",gridTemplateColumns:"repeat(3,1fr)",textAlign:'center',marginTop:"10px",padding:'15px',border:'2px solid rgb(70,128,255)',borderRadius:"10px"}} >
+        <div style={{display:"grid",gridTemplateColumns:"repeat(3,1fr)",textAlign:'center',marginTop:"10px",padding:'15px',border:'2px solid rgb(70,128,255)',borderRadius:"10px"}}  className="bg-blue-500 text-white dark:text-white"  >
                  <div>
-                 <p className="text-xl font-medium text-black dark:text-white">0</p>
-                 <p className="text-xl font-medium text-black dark:text-white">Male</p>
+                 <p className="text-xl font-medium text-white dark:text-white">{load?<Spinner/>:male}</p>
+                 <p className="text-xl font-medium text-white dark:text-white">Male</p>
 
                  </div>
                  <div>
-                 <p className="text-xl font-medium text-black dark:text-white">0</p>
-                 <p className="text-xl font-medium text-black dark:text-white">Female</p>
+                 <p className="text-xl font-medium text-white dark:text-white">{load?<Spinner/>:female}</p>
+                 <p className="text-xl font-medium text-white dark:text-white">Female</p>
                  </div>
                  <div>
-                 <p className="text-xl font-medium text-black dark:text-white">0</p>
-                 <p className="text-xl font-medium text-black dark:text-white">Other</p>
+                 <p className="text-xl font-medium text-white dark:text-white">{load?<Spinner/>:other}</p>
+                 <p className="text-xl font-medium text-white dark:text-white">Other</p>
                  </div>
             </div>
       </div>
