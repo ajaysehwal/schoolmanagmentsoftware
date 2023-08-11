4919faf09fdb1acaa94d49ba9c38855d3ea62b67
ajaysehwal <ajaysehwal000@gmail.com>
2023-08-11 21:08:30 +0530

    updated

--- client/src/components/CardFive.tsx
+++ client/src/components/CardFive.tsx
@@ -1,8 +1,45 @@
+import { Spinner } from "@material-tailwind/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
+
+
 const CardFive = () => {
+  const cookies = new Cookies();
+  const auth = cookies.get('_UID');
+  const [male,setmale]=useState(0);
+  const [load,setload]=useState(false);
+  const [female,setfemale]=useState(0);
+  const [other,setother]=useState(0);
+
+  const getdatacount=async(auth:any)=>{
+    try{
+      const res=await axios.get(`http://localhost:8000/schoolstudent/${auth}/male`)
+        setmale(res.data.length);
+    }catch(err){
+      return err;
+    }
+    try{
+      const res=await axios.get(`http://localhost:8000/schoolstudent/${auth}/female`)
+   setfemale(res.data.length)
+    }catch(err){
+      return err;
+    }
+    try{
+      const res=await axios.get(`http://localhost:8000/schoolstudent/${auth}/other`)
+   setother(res.data.length)
+    }catch(err){
+      return err;
+    }
+  }
+  useEffect(()=>{
+ getdatacount(auth);
+  },[])
+
     return (
       <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
       
-   <p className="text-title-md font-bold text-black dark:text-white">4546</p>
+   <p className="text-title-md font-bold text-black dark:text-white">{load?<Spinner/>:male+female+other}</p>
         {/* </div> */}
   
         <div className="mt-4 flex items-end justify-between">
@@ -33,19 +70,19 @@ const CardFive = () => {
             </svg>
           </span> */}
         </div>
-        <div style={{display:"grid",gridTemplateColumns:"repeat(3,1fr)",textAlign:'center',marginTop:"10px",padding:'15px',border:'2px solid rgb(156,204,101)',borderRadius:"10px"}} >
+        <div style={{display:"grid",gridTemplateColumns:"repeat(3,1fr)",textAlign:'center',marginTop:"10px",padding:'15px',borderRadius:"10px"}} className="bg-green-500 text-white dark:text-white" >
                  <div>
-                 <p className="text-xl font-medium text-black dark:text-white">0</p>
-                 <p className="text-xl font-medium text-black dark:text-white">Male</p>
+                 <p className="text-xl font-medium text-white dark:text-white">{load?<Spinner/>:male}</p>
+                 <p className="text-xl font-medium text-white dark:text-white">Male</p>
 
                  </div>
                  <div>
-                 <p className="text-xl font-medium text-black dark:text-white">0</p>
-                 <p className="text-xl font-medium text-black dark:text-white">Female</p>
+                 <p className="text-xl font-medium text-white dark:text-white">{load?<Spinner/>:female}</p>
+                 <p className="text-xl font-medium text-white dark:text-white">Female</p>
                  </div>
                  <div>
-                 <p className="text-xl font-medium text-black dark:text-white">0</p>
-                 <p className="text-xl font-medium text-black dark:text-white">Other</p>
+                 <p className="text-xl font-medium text-white dark:text-white">{load?<Spinner/>:other}</p>
+                 <p className="text-xl font-medium text-white dark:text-white">Other</p>
                  </div>
             </div>
       </div>
