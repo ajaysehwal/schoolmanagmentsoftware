1f5f9fac4c222f278e4d9d6673e91abe842bd067
ajaysehwal <ajaysehwal000@gmail.com>
2023-08-11 21:11:19 +0530

    updated

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -2311,10 +2311,10 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                   return (
                     <React.Fragment>
                       <NavLink
-                        to="#"
+                        to="/operations"
                         className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                          (pathname === '#' ||
-                            pathname.includes('dashboard')) &&
+                          (pathname === 'operations' ||
+                            pathname.includes('operations')) &&
                           'bg-graydark dark:bg-meta-4'
                         }`}
                         onClick={(e) => {
@@ -2378,7 +2378,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           </li>
                           <li>
                             <NavLink
-                              to="/operations"
+                              to="/operations/announcement"
                               className={({ isActive }) =>
                                 'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
                                 (isActive && '!text-white')
